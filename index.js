2b1c3d1c2b572815b5ff320416c9a2a35eb74631
mr-vic <mr.vic@bk.ru>
2022-06-13 17:12:48 +0500

    save

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-import express from 'express'
-import mongoose from 'mongoose'
+/* eslint-disable no-console */
+import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 5000;
-const DB_URL = ``
+const DB_URL = '';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post('/', (req, res) => {
-    //console.log(req.query)
-    console.log(req.body)
-    res.status(200).json('Server is working!')
-})
+  // console.log(req.query)
+  console.log(req.body);
+  res.status(200).json('Server is working!');
+});
 
 async function startApp() {
-    try {
-        await mongoose.connect(DB_URL)
-        app.listen(PORT, () => console.log('SERVER STARTDED ON PORT ' + PORT))
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log(`SERVER STARTDED ON PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-startApp()
+startApp();
