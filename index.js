de91e61a1bc26671de4be02e655a2be422179545
mr-vic <mr.vic@bk.ru>
2022-06-21 22:51:47 +0500

    crud post with service logic

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-/* eslint-disable no-console */
+import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
+import router from './router.js';
+import fileUpload from 'express-fileupload';
 
-const PORT = 5000;
-const DB_URL = '';
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL || '';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/', (req, res) => {
-  // console.log(req.query)
-  console.log(req.body);
-  res.status(200).json('Server is working!');
-});
+app.use('/api', router);
+app.use(fileUpload());
 
 async function startApp() {
   try {
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
     app.listen(PORT, () => console.log(`SERVER STARTDED ON PORT ${PORT}`));
   } catch (e) {
     console.log(e);
